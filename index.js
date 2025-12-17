78c3e842bb3cf1665e41f3e68d44947cdaf68623
Poojan59 <poojansoni39@gmail.com>
2025-12-17 14:01:03 +0530

    Week 3 Day 1-3  backend integration completed

--- index.js
+++ index.js
@@ -2,20 +2,38 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5009;
-
-app.use(cors()); // allow frontend
+app.use(cors());
 app.use(express.json());
 
-app.get("/api/users", (req, res) => {
-  res.json([
-    { id: 1, name: "Poojan" },
-    { id: 2, name: "Manan" },
-    { id: 3, name: "Sneakers Elite User" },
-    { id: 4, name: "Virpal"}
-  ]);
+let users = [
+  { id: 1, name: "Nike-T-shirt" },
+  { id: 2, name: "Nike-watch" }
+];
+
+// GET API
+app.get("/users", (req, res) => {
+  res.json(users);
 });
 
-app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
+// POST API
+app.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: "Name is required" });
+  }
+
+  const newUser = {
+    id: users.length + 1,
+    name
+  };
+
+  users.push(newUser);
+  console.log("New user added:", newUser);
+
+  res.json({ message: "User added successfully", user: newUser });
+});
+
+app.listen(5009, () => {
+  console.log("Backend running on http://localhost:5009");
 });
